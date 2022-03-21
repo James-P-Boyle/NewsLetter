b87f466687c914f7e825ddb538afba9ff2dee9ad
James Boyle <james.patrick.boyle92@gmail.com>
2022-03-21 14:44:44 +0100

    Add list component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
-//import Header
-//import List
+import List from "./components/List";
 
 function App() {
-  return <div className="App">gergrgr</div>;
+  return (
+    <div className="App">
+      <List />
+    </div>
+  );
 }
 
 export default App;
-
-//Header --- input + button
-//List --- map over list items
-
-//ListItems --- how each item in the list will be displayed (<h1>{props.title}.....)
