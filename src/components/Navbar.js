ed6c757d781cae01a4ac22fa7ba454fc3122a825
James Boyle <james.patrick.boyle92@gmail.com>
2022-03-25 17:03:23 +0100

    Add style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import Header from "../Header";
 export default function Navbar(props) {
   return (
     <div className="navbar">
-      <h2>LOGO</h2>
       <Header setQuery={props.setQuery} />
     </div>
   );
